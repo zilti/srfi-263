faa4467475ff3020ea9cf2066eff895848d830a5
Daniel Ziltener <dziltener@lyrion.ch>
2026-04-19 01:19:56 +0200

    Fixing exported identifiers.

--- srfi-263/srfi-263.scm
+++ srfi-263/srfi-263.scm
@@ -1,3 +1,7 @@
 (define-library (srfi 263)
-  (export *the-root-object*)
+  (export *the-root-object*
+          slot?
+          slot-getter
+          slot-setter
+          slot-type)
   (include "srfi-263.impl.scm"))
